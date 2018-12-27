c95fea142a86cbb4b027d7c06bc791596651bba9
cholzkorn <c.holzkorn@hotmail.com>
2018-12-27 12:10:13 +0100

    Added user input controls

--- mars-rover/mars_rover.py
+++ mars-rover/mars_rover.py
@@ -19,12 +19,42 @@ class MarsRover:
 # Creating an instance
 rover1 = MarsRover()
 
-# Checking
-print(rover1.position)
+# Control function
 
-# Moving
-rover1.move_north()
-rover1.move_west()
+def control():
+    direction = input(
+        'Mars Rover Controls: north, south, west, east, position, '
+        'stop: ')
+    if direction == 'north':
+        rover1.move_north()
+        return control()
+    elif direction == 'south':
+        rover1.move_south()
+        return control()
+    elif direction == 'west':
+        rover1.move_west()
+        return control()
+    elif direction == 'east':
+        rover1.move_east()
+        return control()
+    elif direction == 'position':
+        print(rover1.position)
+        return control()
+    elif direction == 'stop':
+        print('Rover movement stopped.')
+        prompt = input('Do you want to end the program (y/n)? Press any other '
+                       'Key to abort.')
+        if prompt == 'n':
+            return control()
+        elif prompt == 'y':
+            print('Program ended. Returning position.')
+            return rover1.position
+        else:
+            print('Stop aborted. Returning to control.')
+            return control()
+    else:
+        print('Wrong input. Returning to control.')
+        return control()
 
-# Checking
-print(rover1.position)
+# Calling function
+control()
