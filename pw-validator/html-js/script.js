6469a89d6545aae54e3a0262ba681f59d56d1fa3
cholzkorn <c.holzkorn@hotmail.com>
2019-02-01 22:03:56 +0100

    Completed pw challenge in HTML + JS

--- pw-validator/html-js/script.js
+++ pw-validator/html-js/script.js
@@ -1,31 +1,30 @@
-function validate(pw){
-  var pw = parseInt(pw);
-  var pw_length = pw.length;
+function validate(pass){
+  var check = pass.toString();
+  var check_length = check.length;
 
   // Defining regular expressions
   var numbers = RegExp("[0-9]");
   var special = RegExp("[^A-Za-z0-9]");
 
   // matching regular expressions
-  console.log(numbers.test(pw));
+  console.log(numbers.test(check));
 
-  var number_found = numbers.test(pw);
-  var special_found = special.test(pw);
+  var number_found = numbers.test(check);
+  var special_found = special.test(check);
+
+
 
   // conditionals
-  if (pw_length < 5){
+  if (check_length < 5){
     document.getElementById('output').innerHTML = "Please enter a password with at least 5 characters.";
   }
-  else if (pw_length > 10){
+  else if (check_length > 10){
     document.getElementById('output').innerHTML = "Please enter a password with less than 10 characters.";
   }
-  else if (pw > 10){
-    document.getElementById('output').innerHTML = "Please enter a password with less than 10 characters.";
-  }
-  else if (length(number_found) < 0){
+  else if (number_found == false){
     document.getElementById('output').innerHTML = "Your password must contain a number.";
   }
-  else if (length(special_found) < 0){
+  else if (special_found == false){
     document.getElementById('output').innerHTML = "Your password must contain a special character.";
   }
   else{
